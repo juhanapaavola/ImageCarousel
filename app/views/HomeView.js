58d252680ffcb83be1baffaea967a02d06bb22f9
juhanapaavola <juhana.paavola@gmail.com>
2014-04-05 11:01:55 +0300

    add query

--- app/views/HomeView.js
+++ app/views/HomeView.js
@@ -6,6 +6,7 @@ define(function(require){
 	Backbone = require('backbone'),
 	tpl = require('text!templates/Home.html'),
 	template = _.template(tpl);
+	var jQuery = $;
 	var flexslider = require('flexslider');
 	var Badge = require('views/InfoBadgeView');
 
